b811b7afd98090ce5eba2fd7c53cad159ed74c4a
Lex <gipro007@mail.ru>
2021-09-27 19:22:43 +1000

    Долбим гит

---
+++ Script.js
@@ -0,0 +1,15 @@
+const numberOfIlm = +prompt('Сколько фильмов вы уже посмотрели?','');
+const personalMovieDB = {
+count: numberOfIlm,
+movies: {},
+actors: {},
+genres: [],
+private: false
+};
+const a = prompt('Один из последних просмотренных фильмов?','');
+const b = prompt('а сколько оцените его?','');
+
+const movies = {
+    a:b
+};
+console.log('++');
